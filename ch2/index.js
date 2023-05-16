14be4e9139b17c0120bc10fa85fffa227d929618
blockodyssey-uhui <reah@blockodyssey.io>
2023-05-16 16:45:23 +0900

    first commit

---
+++ ch2/index.js
@@ -0,0 +1,69 @@
+import CanvasOption from "./js/CanvasOption.js";
+import Particle from "./js/Particle.js";
+import { randomNumBetween } from "./js/utils.js";
+
+class Canvas extends CanvasOption {
+    constructor() {
+        super();
+        this.particles = [];
+    }
+
+    init() {
+        this.canvasWidth = innerWidth;
+        this.canvasHeight = innerHeight;
+        this.canvas.width = this.canvasWidth * this.dpr;
+        this.canvas.height = this.canvasHeight * this.dpr;
+        this.ctx.scale(this.dpr, this.dpr);
+
+        this.canvas.style.width = this.canvasWidth + "px";
+        this.canvas.style.height = this.canvasHeight + "px";
+
+        this.createParticles();
+    }
+
+    createParticles() {
+        const PARTICLE_NUM = 10;
+        const x = randomNumBetween(0, this.canvasWidth);
+        const y = randomNumBetween(0, this.canvasHeight);
+
+        for (let i = 0; i < PARTICLE_NUM; i++) {
+            const vx = randomNumBetween(-5, 5);
+            const vy = randomNumBetween(-5, 5);
+            this.particles.push(new Particle(x, y, vx, vy));
+        }
+    }
+
+    render() {
+        let now, delta;
+        let then = Date.now();
+
+        const frame = () => {
+            requestAnimationFrame(frame);
+            now = Date.now();
+            delta = now - then;
+            if (delta < this.interval) return;
+
+            this.ctx.fillStyle = this.bgColor;
+            this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+            this.particles.forEach((particle) => {
+                particle.update();
+                particle.draw();
+            });
+
+            then = now - (delta % this.interval);
+        };
+        requestAnimationFrame(frame);
+    }
+}
+
+const canvas = new Canvas();
+
+window.addEventListener("load", () => {
+    canvas.init();
+    canvas.render();
+});
+
+window.addEventListener("resize", () => {
+    canvas.init();
+});
---
+++ ch2/js/Particle.js
@@ -0,0 +1,23 @@
+import CanvasOption from "./CanvasOption.js";
+
+export default class Particle extends CanvasOption {
+    constructor(x, y, vx, vy) {
+        super();
+
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+    }
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+    draw() {
+        this.ctx.fillStyle = "#ffffff";
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+}
