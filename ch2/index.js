e4a10d42bd7eb7664a24fc300c5bc7419a1ad9bd
blockodyssey-uhui <reah@blockodyssey.io>
2023-05-25 09:32:01 +0900

    feat : 스파크생성 , 불꽃놀이 끝

--- ch2/index.js
+++ ch2/index.js
@@ -2,13 +2,14 @@ import CanvasOption from "./js/CanvasOption.js";
 import Particle from "./js/Particle.js";
 import Tail from "./js/Tail.js";
 import { hypotenuse, randomNumBetween } from "./js/utils.js";
-
+import Spark from "./js/Spark.js";
 class Canvas extends CanvasOption {
     constructor() {
         super();
 
         this.tails = [];
         this.particles = [];
+        this.sparks = [];
     }
 
     init() {
@@ -27,12 +28,12 @@ class Canvas extends CanvasOption {
     createTail() {
         const x = randomNumBetween(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
         const vy = this.canvasHeight * randomNumBetween(0.01, 0.015) * -1;
-        const color = "255,255,255";
+        const colorDeg = randomNumBetween(0, 360);
 
-        this.tails.push(new Tail(x, vy, color));
+        this.tails.push(new Tail(x, vy, colorDeg));
     }
 
-    createParticles(x, y, color) {
+    createParticles(x, y, colorDeg) {
         const PARTICLE_NUM = 400;
 
         for (let i = 0; i < PARTICLE_NUM; i++) {
@@ -41,8 +42,8 @@ class Canvas extends CanvasOption {
             const vx = r * Math.cos(angle);
             const vy = r * Math.sin(angle);
             const opacity = randomNumBetween(0.6, 0.9);
-
-            this.particles.push(new Particle(x, y, vx, vy, opacity, color));
+            const _colorDeg = randomNumBetween(-20, 20) + colorDeg;
+            this.particles.push(new Particle(x, y, vx, vy, opacity, _colorDeg));
         }
     }
 
@@ -59,21 +60,40 @@ class Canvas extends CanvasOption {
             this.ctx.fillStyle = this.bgColor + "40";
             this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
+            this.ctx.fillStyle = `rgba(255,255,255, ${this.particles.length / 50000})`;
+            this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
             if (Math.random() < 0.03) this.createTail();
 
             this.tails.forEach((tail, index) => {
                 tail.update();
                 tail.draw();
 
+                for (let i = 0; i < Math.round(-tail.vy * 0.5); i++) {
+                    const vx = randomNumBetween(-5, 5) * 0.05;
+                    const vy = randomNumBetween(-5, 5) * 0.05;
+                    const opacity = Math.min(-tail.vy, 0.5);
+                    this.sparks.push(new Spark(tail.x, tail.y, vx, vy, opacity, tail.colorDeg));
+                }
+
                 if (tail.vy > -0.7) {
                     this.tails.splice(index, 1);
-                    this.createParticles(tail.x, tail.y, tail.color);
+                    this.createParticles(tail.x, tail.y, tail.colorDeg);
                 }
             });
             this.particles.forEach((particle, index) => {
                 particle.update();
                 particle.draw();
+                if (Math.random() < 0.1) this.sparks.push(new Spark(particle.x, particle.y, 0, 0, 0.3, 45));
+
                 if (particle.opacity < 0) this.particles.splice(index, 1);
+            });
+
+            this.sparks.forEach((spark, index) => {
+                spark.update();
+                spark.draw();
+
+                if (spark.opacity < 0) this.sparks.splice(index, 1);
             });
 
             then = now - (delta % this.interval);
--- ch2/js/Spark.js
+++ ch2/js/Spark.js
@@ -1,6 +1,6 @@
 import CanvasOption from "./CanvasOption.js";
 
-export default class Particle extends CanvasOption {
+export default class Spark extends CanvasOption {
     constructor(x, y, vx, vy, opacity, colorDeg) {
         super();
 
@@ -8,26 +8,22 @@ export default class Particle extends CanvasOption {
         this.y = y;
         this.vx = vx;
         this.vy = vy;
+
         this.opacity = opacity;
-        this.gravity = 0.12;
-        this.friction = 0.93;
         this.colorDeg = colorDeg;
     }
-    update() {
-        this.vy += this.gravity;
 
-        this.vx *= this.friction;
-        this.vy *= this.friction;
+    update() {
+        this.opacity -= 0.01;
 
         this.x += this.vx;
         this.y += this.vy;
-
-        this.opacity -= 0.02;
     }
+
     draw() {
-        this.ctx.fillStyle = `hsla(${this.color} ,100%,65%, ${this.opacity})`;
         this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+        this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
+        this.ctx.fillStyle = `hsla(${this.colorDeg},100%,65%,${this.opacity})`;
         this.ctx.fill();
         this.ctx.closePath();
     }
